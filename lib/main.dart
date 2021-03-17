e76583e01fc408f6e18d2e810b91719ab052bdd5
Dipak <dipak.prajapati@bacancy.com>
2021-03-17 13:11:22 +0530

    transformed image saved on device

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:svgonvideo/video.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
+import 'dart:ui' as ui;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,7 +27,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TransformDemo extends StatelessWidget {
+class TransformDemo extends StatefulWidget {
+  @override
+  _TransformDemoState createState() => _TransformDemoState();
+}
+
+class _TransformDemoState extends State<TransformDemo> {
+  GlobalKey _globalKey = new GlobalKey();
+
+  Future<String> _capturePng() async {
+    try {
+      print('inside');
+      RenderRepaintBoundary boundary =
+          _globalKey.currentContext.findRenderObject();
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      ByteData byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      final buffer = byteData.buffer;
+
+      final filename = '${DateTime.now().millisecondsSinceEpoch}_svg.png';
+
+      Directory dir = await getExternalStorageDirectory();
+
+      print(dir.path + '/' + filename);
+
+      final path = dir.path + "/" + filename;
+
+      await new File(path)
+          .writeAsBytes(buffer.asUint8List(
+              byteData.offsetInBytes, byteData.lengthInBytes))
+          .then((value) => print('written'));
+
+      File file = File(path);
+      print('Loaded file ${file.path}');
+
+      return file.path;
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final ValueNotifier<Matrix4> notifier = ValueNotifier(Matrix4.identity());
@@ -29,28 +75,28 @@ class TransformDemo extends StatelessWidget {
         onMatrixUpdate: (m, tm, sm, rm) {
           notifier.value = m;
         },
-        child: AnimatedBuilder(
-          animation: notifier,
-          builder: (ctx, child) {
-            return Transform(
-              transform: notifier.value,
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    color: Colors.white30,
-                  ),
-                  SvgPicture.asset('assets/panda.svg',
-                      semanticsLabel: 'Acme Logo')
-                ],
-              ),
-            );
-          },
+        child: RepaintBoundary(
+          key: _globalKey,
+          child: AnimatedBuilder(
+            animation: notifier,
+            builder: (ctx, child) {
+              return Transform(
+                transform: notifier.value,
+                child: Stack(
+                  children: <Widget>[
+                    Container(),
+                    SvgPicture.asset('assets/panda.svg',
+                        semanticsLabel: 'Acme Logo')
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('--------------------------');
-          print(notifier.value);
+        onPressed: () async {
+          final res = await _capturePng();
         },
         child: Icon(Icons.add),
       ),
