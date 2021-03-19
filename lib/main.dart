4874a91a06490f20b2badee24c4323f4ecda1247
Dipak <dipak.prajapati@bacancy.com>
2021-03-19 17:25:07 +0530

    rotate and translation done (Without Scale)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +8,7 @@ import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:svgonvideo/video.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
-import 'dart:ui' as ui;
 import 'package:path_provider/path_provider.dart';
-
-import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -40,14 +35,11 @@ class TransformDemo extends StatefulWidget {
 class _TransformDemoState extends State<TransformDemo> {
   GlobalKey _globalKey = new GlobalKey();
 
-  final _stickerkey = new GlobalKey();
-
-  double _angel = 0.0;
+  double _rotation = 0.0;
 
   Offset _offset = Offset(0, 0);
 
-  double _xScale = 0;
-  double _yScale = 0;
+  double _scale = 1;
 
   _loadSvgFromAsset() async {
     final filename = 'panda.png';
@@ -71,27 +63,17 @@ class _TransformDemoState extends State<TransformDemo> {
         await FilePicker.platform.pickFiles(type: FileType.video);
 
     print(result?.files.single.path);
-
     String videoPath = result?.files.single.path;
 
     final filename = '${DateTime.now().millisecondsSinceEpoch}_output.mp4';
-
     Directory dir = await getExternalStorageDirectory();
 
     final imgpath = await _loadSvgFromAsset();
 
-    // print(dir.path + '/' + filename);
     final outputPath = dir.path + "/" + filename;
 
-    // String imgOverlayCommand =
-    //     '-i $videoPath -i imagepath -filter_complex "[0:v][1:v] overlay=-300:-300" $outputPath';
-
-    // String cmd =
-    //     '''-i $videoPath -i $imgpath -filter_complex "[1:v] rotate=$_angel:c=none:ow=rotw(iw):oh=roth(ih) [rotate];[0:v][rotate] overlay=${_offset.dx}:${_offset.dx}" -codec:a copy $outputPath''';
-
     final localCommand =
-        "-i '$videoPath' -i '$imgpath' -filter_complex '[1:v]format=bgra,rotate=$_angel:c=none:ow=rotw($_angel):oh=roth($_angel)[rotate];[rotate]scale=150:150[scale];[0:v][scale] overlay=${_offset.dx}:${_offset.dy}' -q:v 0 -q:a 0 -r 60 '$outputPath'";
-
+        "-i '$videoPath' -i '$imgpath' -filter_complex '[1:v]format=bgra,rotate=$_rotation:c=none:ow=rotw($_rotation):oh=roth($_rotation)[rotate];[rotate]scale=300:300[scale];[0:v][scale] overlay=${_offset.dx * 2}:${_offset.dy * 2}' -q:v 0 -q:a 0 -r 60 '$outputPath'";
     print('------------- $localCommand');
 
     FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg();
@@ -110,27 +92,17 @@ class _TransformDemoState extends State<TransformDemo> {
     final ValueNotifier<Matrix4> notifier = ValueNotifier(Matrix4.identity());
     return Scaffold(
       body: MatrixGestureDetector(
+        clipChild: true,
+        shouldScale: false,
         focalPointAlignment: Alignment.center,
         onMatrixUpdate: (m, tm, sm, rm) {
           notifier.value = m;
 
-          final rotation = m.getRotation();
-          final degree = asin(rotation[1]) * pi / 180;
-          final angel = degree < 0 ? (360 + degree) : degree;
+          final val = MatrixGestureDetector.decomposeToValues(m);
 
-          // setting parameters
-          _xScale = m.getRow(0)[0];
-          _yScale = m.getRow(0)[0];
-          _angel = degree * 180 / pi;
-          // _offset = Offset(traslation[0], traslation[1]);
-
-          final RenderBox box = _stickerkey.currentContext.findRenderObject();
-
-          _offset = box.localToGlobal(Offset.zero);
-          // print(box.size);
-          // print('--------------------');
-          // print(rm);
-          print(_angel);
+          _offset = val.translation;
+          _scale = val.scale;
+          _rotation = val.rotation;
         },
         child: AnimatedBuilder(
           animation: notifier,
@@ -143,8 +115,8 @@ class _TransformDemoState extends State<TransformDemo> {
                   SvgPicture.asset(
                     'assets/panda.svg',
                     semanticsLabel: 'Acme Logo',
-                    height: 200,
-                    key: _stickerkey,
+                    height: 150,
+                    width: 150,
                   )
                 ],
               ),
